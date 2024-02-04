69a456bcc5c743540cbb89abaa94c944b5a49271
alibrahimzada <alibrahimzada@gmail.com>
2024-02-04 11:52:07 -0600

    update query execution

--- src/preprocessing/execute_queries.py
+++ src/preprocessing/execute_queries.py
@@ -28,6 +28,34 @@ def main(args):
     if args.query_name == 'get_fields':
         os.system(f"codeql query run -d ../../databases/{args.database_name} -- get_fields.ql | tail -n +3 >> ../../data/query_outputs/{args.project_name}/{args.project_name}_fields.txt")
 
+    if args.query_name == 'get_interfaces':
+        os.system(f"codeql query run -d ../../databases/{args.database_name} -- get_interfaces_1.ql | tail -n +3 >> ../../data/query_outputs/{args.project_name}/{args.project_name}_interfaces.txt")
+        os.system(f"codeql query run -d ../../databases/{args.database_name} -- get_interfaces_2.ql | tail -n +3 >> ../../data/query_outputs/{args.project_name}/{args.project_name}_interfaces.txt")
+
+    if args.query_name == 'get_nested_classes':
+        os.system(f"codeql query run -d ../../databases/{args.database_name} -- get_nested_classes.ql | tail -n +3 >> ../../data/query_outputs/{args.project_name}/{args.project_name}_nested_classes.txt")
+
+    if args.query_name == 'get_superclasses':
+        os.system(f"codeql query run -d ../../databases/{args.database_name} -- get_superclasses_1.ql | tail -n +3 >> ../../data/query_outputs/{args.project_name}/{args.project_name}_superclasses.txt")
+        os.system(f"codeql query run -d ../../databases/{args.database_name} -- get_superclasses_2.ql | tail -n +3 >> ../../data/query_outputs/{args.project_name}/{args.project_name}_superclasses.txt")
+
+    if args.query_name == 'get_parameters':
+        os.system(f"codeql query run -d ../../databases/{args.database_name} -- get_parameters_1.ql | tail -n +3 >> ../../data/query_outputs/{args.project_name}/{args.project_name}_parameters.txt")
+        os.system(f"codeql query run -d ../../databases/{args.database_name} -- get_parameters_2.ql | tail -n +3 >> ../../data/query_outputs/{args.project_name}/{args.project_name}_parameters.txt")
+        os.system(f"codeql query run -d ../../databases/{args.database_name} -- get_parameters_3.ql | tail -n +3 >> ../../data/query_outputs/{args.project_name}/{args.project_name}_parameters.txt")
+
+    if args.query_name == 'get_class_callables':
+        os.system(f"codeql query run -d ../../databases/{args.database_name} -- get_class_callables_1.ql | tail -n +3 >> ../../data/query_outputs/{args.project_name}/{args.project_name}_class_callables.txt")
+        os.system(f"codeql query run -d ../../databases/{args.database_name} -- get_class_callables_2.ql | tail -n +3 >> ../../data/query_outputs/{args.project_name}/{args.project_name}_class_callables.txt")
+        os.system(f"codeql query run -d ../../databases/{args.database_name} -- get_class_callables_3.ql | tail -n +3 >> ../../data/query_outputs/{args.project_name}/{args.project_name}_class_callables.txt")
+        os.system(f"codeql query run -d ../../databases/{args.database_name} -- get_class_callables_4.ql | tail -n +3 >> ../../data/query_outputs/{args.project_name}/{args.project_name}_class_callables.txt")
+
+    if args.query_name == 'get_call_graph':
+        os.system(f"codeql query run -d ../../databases/{args.database_name} -- get_call_graph.ql | tail -n +3 >> ../../data/query_outputs/{args.project_name}/{args.project_name}_call_graph.txt")
+
+    if args.query_name == 'get_types':
+        os.system(f"codeql query run -d ../../databases/{args.database_name} -- get_types.ql | tail -n +3 >> ../../data/query_outputs/{args.project_name}/{args.project_name}_types.txt")
+
     if args.query_name == 'get_overridden_methods':
         os.system(f"codeql query run -d ../../databases/{args.database_name} -- get_overridden_methods.ql | tail -n +3 >> ../../data/query_outputs/{args.project_name}/{args.project_name}_overridden_methods.txt")
 
