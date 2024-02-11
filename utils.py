21f05c292e0cb344d83bb885c250a22ae132b515
alibrahimzada <alibrahimzada@gmail.com>
2024-02-10 19:00:45 -0600

    update dependency parsing script

--- utils.py
+++ utils.py
@@ -6,42 +6,65 @@ import graphviz
 
 
 def parse_dependencies(project_name):
-    """
-    you should have compiled and executed the following before running this function:
-    jdeps -verbose -dotoutput data/dependencies java_projects/<project_name>/target/classes/<path-to-class-files>/*.class
-    rm -rf dependencies/summary.dot
-    """
+    os.makedirs(f'data/dependencies/{project_name}', exist_ok=True)
+
+    class_dependencies = {}
+    java_files = []
+    for root, dirs, files in os.walk(f'java_projects/cleaned_final_projects/{project_name}/src'):
+        for file in files:
+            if file.endswith('.java'):
+                java_files.append(os.path.join(root, file))
+    
+    for dot_class_file in java_files:
+        class_name = dot_class_file.split('/')[-1].split('.')[0]
+        class_dependencies.setdefault(class_name, [])
+    
+    os.system(f'jdeps -verbose -dotoutput data/dependencies/{project_name} java_projects/cleaned_final_projects/{project_name}/target/classes')
+    os.system(f'jdeps -verbose -dotoutput data/dependencies/{project_name} java_projects/cleaned_final_projects/{project_name}/target/test-classes')
+    os.remove(f'data/dependencies/{project_name}/summary.dot')
+
     dependencies_dir = os.path.join(os.path.dirname(__file__), f'data/dependencies/{project_name}')
     class_deps = os.listdir(dependencies_dir)
-    class_dependencies = {}
     for class_dep in class_deps:
-
-        if class_dep.startswith('package-info'):
-            continue
-
-        if not class_dep.endswith('.dot'):
-            continue
-
-        if '$' in class_dep:
-            continue
-
-        current_class = class_dep.split('.')[0]
-        class_dependencies.setdefault(current_class, [])
 
         with open(os.path.join(dependencies_dir, class_dep), 'r') as f:
             lines = f.readlines()
             for line in lines[2:-1]:
 
                 candidate_line = line.strip()
-                class_name = re.search(r'\((.*?)\)', candidate_line).group(1).split('.')[0]
+                class_name = re.search(r'->\s(.*?)\s\(', candidate_line).group(1)
+                class_name = class_name.split('.')[-1].strip()
 
-                if class_name in ['java', 'java.base', 'not found']:
+                current_class = candidate_line[candidate_line.find('\"') + 1:candidate_line.find('\"', candidate_line.find('\"') + 1)]
+                current_class = current_class.split('.')[-1].strip()
+
+                if 'java.base' in candidate_line or 'not found' in candidate_line or 'java' in candidate_line:
                     continue
 
                 if '$' in class_name:
-                    class_name = class_name.split('$')[0]
+                    if class_name.split('$')[-1].isdigit():
+                        # class_name = '.'.join(class_name.split('$')[:-1])
+                        class_name = class_name.split('$')[0]
+                        class_dependencies.setdefault(class_name, [])
+                    else:
+                        # class_name = class_name.replace('$', '.')
+                        class_name = class_name.split('$')[-1]
+                        class_dependencies.setdefault(class_name, [])
+                
+                if '$' in current_class:
+                    if current_class.split('$')[-1].isdigit():
+                        # current_class = '.'.join(current_class.split('$')[:-1])
+                        current_class = current_class.split('$')[0]
+                        class_dependencies.setdefault(current_class, [])
+                    else:
+                        # current_class = current_class.replace('$', '.')
+                        current_class = current_class.split('$')[-1]
+                        class_dependencies.setdefault(current_class, [])
                 
                 if class_name == current_class:
+                    continue
+                
+                if class_name in class_dependencies[current_class]:
                     continue
 
                 class_dependencies[current_class].append(class_name)
@@ -49,38 +72,37 @@ def parse_dependencies(project_name):
     with open(os.path.join(dependencies_dir, 'dependencies.json'), 'w') as f:
         json.dump(class_dependencies, f, indent=4)
 
-    # uncomment below to plot dependency graph
-    g = graphviz.Digraph('G', filename=os.path.join(dependencies_dir, 'dependencies.gv'), format='png')
+    g = graphviz.Digraph('G', filename=os.path.join(dependencies_dir, 'dependencies.gv'))
     for class_name, dependencies in class_dependencies.items():
         for dependency in dependencies:
             g.edge(class_name, dependency)
-    g.view()
+    g.render(os.path.join(dependencies_dir, 'dependency_graph'), format='png', cleanup=True)
 
-    # remove cycles
-    removed_cycles = []
-    for k,v in class_dependencies.items():
-        for dependency in v:
-            if k in class_dependencies[dependency]:
-                class_dependencies[dependency].remove(k)
-                removed_cycles.append((dependency, k))
+    # # remove cycles
+    # removed_cycles = []
+    # for k,v in class_dependencies.items():
+    #     for dependency in v:
+    #         if k in class_dependencies[dependency]:
+    #             class_dependencies[dependency].remove(k)
+    #             removed_cycles.append((dependency, k))
 
-    # create traversal based on dependencies (pure to impure)
-    traversal = {}
-    while len(class_dependencies) != len(traversal):
-        for k,v in class_dependencies.items():
-            if k not in traversal:
-                if len(v) == 0:
-                    traversal[k] = []
-                else:
-                    if all([x in traversal for x in v]):
-                        traversal[k] = v
+    # # create traversal based on dependencies (pure to impure)
+    # traversal = {}
+    # while len(class_dependencies) != len(traversal):
+    #     for k,v in class_dependencies.items():
+    #         if k not in traversal:
+    #             if len(v) == 0:
+    #                 traversal[k] = []
+    #             else:
+    #                 if all([x in traversal for x in v]):
+    #                     traversal[k] = v
 
-    # add back cycles after creating traversal
-    for i,j in removed_cycles:
-        traversal[i].append(j)
+    # # add back cycles after creating traversal
+    # for i,j in removed_cycles:
+    #     traversal[i].append(j)
 
-    with open(os.path.join(dependencies_dir, 'traversal.json'), 'w') as f:
-        json.dump(traversal, f, indent=4)
+    # with open(os.path.join(dependencies_dir, 'traversal.json'), 'w') as f:
+    #     json.dump(traversal, f, indent=4)
 
 
 def main(args):
