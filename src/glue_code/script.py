4be4e6ceb2f4455d0bae5562339f669f646dad17
Salman Abid <salman-abid@hotmail.com>
2024-02-16 21:24:48 +0500

    add initial glue automation

---
+++ src/glue_code/script.py
@@ -0,0 +1,97 @@
+import argparse
+import os
+import json
+
+def main(args):
+    schemas = os.listdir(f'data/schemas/{args.project_name}')
+
+    for schema in schemas:
+        if schema.endswith('_python_partial.json'):
+            continue
+
+        # !! specify schema to test !!
+        # if not schema.endswith('.OptionGroup.json'):
+        #     continue
+
+        with open(f'data/schemas/{args.project_name}/{schema}') as f:
+            data = json.load(f)
+        
+        final_glue_code = ""
+
+        formatted_proj_name = args.project_name.replace('-', '.')
+
+        final_glue_code += f"package org.apache.{formatted_proj_name};\n\n"
+        # step 0: add graal imports
+        final_glue_code += "import org.graalvm.polyglot.Value;\n"
+        final_glue_code += "import org.graalvm.polyglot.PolyglotException;\n"
+        final_glue_code += f"import org.apache.{formatted_proj_name}.ContextInitializer;\n"
+
+        # add existing imports
+        for _import in data['imports']:
+            final_glue_code += "".join(data['imports'][_import]["body"])
+
+        # add class definition
+        for _class in data['classes']:
+            lst = []
+            with open(data['path'], 'r') as current_f:
+                lst = current_f.readlines()
+            class_declaration = lst[data['classes'][_class]['start']-1:data['classes'][_class]['end']]
+            final_glue_code += "".join(class_declaration)
+
+            for _field in data['classes'][_class]['fields']:
+                line = "".join(data['classes'][_class]['fields'][_field]['body'])
+                if '=' not in line:
+                    continue
+                final_glue_code += line
+            
+            # add graal attributes
+            python_file = f'"<placeholder>"'
+            class_name = f'"{_class}"'
+            final_glue_code += f"    private static Value clz = ContextInitializer.getPythonClass({python_file}, {class_name});\n"
+            final_glue_code += "    private Value obj;\n"
+
+            # add value constructor
+            final_glue_code += f"    public {_class}(Value obj) {{\n"
+            final_glue_code += "        this.obj = obj;\n"
+            final_glue_code += "    }\n"
+
+            # add getPythonObject()
+            final_glue_code += f"    public Value getPythonObject() {{\n"
+            final_glue_code += "        return obj;\n"
+            final_glue_code += "    }\n"
+
+            for _methods in data['classes'][_class]['methods']:
+                method_body = "".join(data['classes'][_class]['methods'][_methods]['body'])
+                if data['classes'][_class]['methods'][_methods]['is_constructor']:
+                    continue
+
+                # TODO: add comments for dev hints
+                if 'throws' in method_body:
+                    method_signature = method_body[:method_body.find('{')+1]
+                    method_content = method_body[method_body.find('{')+1:method_body.rfind('}')]
+                    
+                    final_glue_code += method_signature + "\n"
+                    final_glue_code += "try {\n"
+                    final_glue_code += method_content
+                    final_glue_code += "} catch (PolyglotException e) {\n"
+                    final_glue_code += "    throw new RuntimeException(e);\n"
+                    final_glue_code += "}\n"
+                    final_glue_code += "}\n"
+
+                else:
+                    final_glue_code += method_body
+
+        final_glue_code += "}\n"
+
+        # TODO: change to structured writing - projects/filenames
+        # write to java file
+        with open('dump.java', 'w') as _f:
+            _f.write(final_glue_code)
+
+        # todo: add shell command to format java file
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Generate glue code for Java')
+    parser.add_argument('--project_name', type=str, dest='project_name', help='name of the project')
+    args = parser.parse_args()
+    main(args)
