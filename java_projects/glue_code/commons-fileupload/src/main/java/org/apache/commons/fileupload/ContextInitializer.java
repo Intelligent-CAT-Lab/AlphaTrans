d3d56a6543367c480e5fe13f95137603e1488bad
Salman Abid <49055716+msalman-abid@users.noreply.github.com>
2024-02-24 00:49:17 +0500

    add commons fileupload glue (#14)
    * add fileupload glue from script
    
    * fix missing declarations

--- java_projects/glue_code/commons-fileupload/src/main/java/org/apache/commons/fileupload/ContextInitializer.java
+++ java_projects/glue_code/commons-fileupload/src/main/java/org/apache/commons/fileupload/ContextInitializer.java
@@ -1,85 +1,159 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.apache.commons.fileupload;
 
 import java.io.File;
-
-import org.graalvm.polyglot.Engine;
 import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Engine;
+import org.graalvm.polyglot.HostAccess;
 import org.graalvm.polyglot.Source;
 import org.graalvm.polyglot.Value;
 
-/**
- * Provides a shared GraalVM engine and a method to get Python classes.
- */
 public abstract class ContextInitializer {
-    /**
-     * The shared GraalVM engine.
-     */
-    private static Engine sharedEngine;
+  private static Engine sharedEngine;
+  private static String codeDirectory = "<placeholder>";
+  private static String packageDirectory = "<placeholder>";
+  private static Context context;
 
-    /**
-     * Directory containing the Python source code.
-     * TODO: change to actual directory
-     */
-    private static String codeDirectory = "../commons-fileupload-translated/src/main/python";
+  static {
+    try {
+      HostAccess hostAccess =
+          HostAccess.newBuilder(HostAccess.ALL)
+            //   .targetTypeMapping(Value.class, MultipartStream.class, (v) -> new MultipartStream(v))
+            //   .targetTypeMapping(
+            //       Value.class,
+            //       MultipartStream.ItemInputStream.class,
+            //       (v) -> new MultipartStream.ItemInputStream(v))
+            //   .targetTypeMapping(
+            //       Value.class,
+            //       MultipartStream.IllegalBoundaryException.class,
+            //       (v) -> new MultipartStream.IllegalBoundaryException(v))
+            //   .targetTypeMapping(
+            //       Value.class,
+            //       MultipartStream.MalformedStreamException.class,
+            //       (v) -> new MultipartStream.MalformedStreamException(v))
+            //   .targetTypeMapping(
+            //       Value.class,
+            //       MultipartStream.ProgressNotifier.class,
+            //       (v) -> new MultipartStream.ProgressNotifier(v))
+            //   .targetTypeMapping(
+            //       Value.class, PortletFileUpload.class, (v) -> new PortletFileUpload(v))
+            //   .targetTypeMapping(Value.class, Streams.class, (v) -> new Streams(v))
+            //   .targetTypeMapping(Value.class, MimeUtility.class, (v) -> new MimeUtility(v))
+            //   .targetTypeMapping(
+            //       Value.class, QuotedPrintableDecoder.class, (v) -> new QuotedPrintableDecoder(v))
+            //   .targetTypeMapping(Value.class, Base64Decoder.class, (v) -> new Base64Decoder(v))
+            //   .targetTypeMapping(
+            //       Value.class,
+            //       InvalidFileNameException.class,
+            //       (v) -> new InvalidFileNameException(v))
+            //   .targetTypeMapping(Value.class, ParseException.class, (v) -> new ParseException(v))
+            //   .targetTypeMapping(
+            //       Value.class, DiskFileItemFactory.class, (v) -> new DiskFileItemFactory(v))
+            //   .targetTypeMapping(
+            //       Value.class, ServletRequestContext.class, (v) -> new ServletRequestContext(v))
+            //   .targetTypeMapping(
+            //       Value.class, MockHttpServletRequest.class, (v) -> new MockHttpServletRequest(v))
+            //   .targetTypeMapping(
+            //       Value.class,
+            //       MockHttpServletRequest.MyServletInputStream.class,
+            //       (v) -> new MockHttpServletRequest.MyServletInputStream(v))
+            //   .targetTypeMapping(Value.class, FileUploadBase.class, (v) -> new FileUploadBase(v))
+            //   .targetTypeMapping(
+            //       Value.class,
+            //       FileUploadBase.FileUploadIOException.class,
+            //       (v) -> new FileUploadBase.FileUploadIOException(v))
+            //   .targetTypeMapping(
+            //       Value.class,
+            //       FileUploadBase.IOFileUploadException.class,
+            //       (v) -> new FileUploadBase.IOFileUploadException(v))
+            //   .targetTypeMapping(
+            //       Value.class,
+            //       FileUploadBase.SizeLimitExceededException.class,
+            //       (v) -> new FileUploadBase.SizeLimitExceededException(v))
+            //   .targetTypeMapping(
+            //       Value.class,
+            //       FileItemIteratorImpl.FileItemStreamImpl.class,
+            //       (v) -> new FileItemIteratorImpl.FileItemStreamImpl(v))
+            //   .targetTypeMapping(
+            //       Value.class,
+            //       FileUploadBase.FileItemIteratorImpl.class,
+            //       (v) -> new FileUploadBase.FileItemIteratorImpl(v))
+            //   .targetTypeMapping(
+            //       Value.class,
+            //       FileUploadBase.FileSizeLimitExceededException.class,
+            //       (v) -> new FileUploadBase.FileSizeLimitExceededException(v))
+            //   .targetTypeMapping(
+            //       Value.class,
+            //       FileUploadBase.SizeException.class,
+            //       (v) -> new FileUploadBase.SizeException(v))
+            //   .targetTypeMapping(
+            //       Value.class,
+            //       FileUploadBase.InvalidContentTypeException.class,
+            //       (v) -> new FileUploadBase.InvalidContentTypeException(v))
+            //   .targetTypeMapping(
+            //       Value.class,
+            //       FileUploadBase.UnknownSizeException.class,
+            //       (v) -> new FileUploadBase.UnknownSizeException(v))
+            //   .targetTypeMapping(Value.class, Util.class, (v) -> new Util(v))
+            //   .targetTypeMapping(
+            //       Value.class, FileItemHeadersImpl.class, (v) -> new FileItemHeadersImpl(v))
+            //   .targetTypeMapping(
+            //       Value.class,
+            //       FileCountLimitExceededException.class,
+            //       (v) -> new FileCountLimitExceededException(v))
+            //   .targetTypeMapping(Value.class, FileUpload.class, (v) -> new FileUpload(v))
+            //   .targetTypeMapping(Value.class, DiskFileItem.class, (v) -> new DiskFileItem(v))
+            //   .targetTypeMapping(Value.class, DefaultFileItem.class, (v) -> new DefaultFileItem(v))
+            //   .targetTypeMapping(
+            //       Value.class,
+            //       FileItemStream.ItemSkippedException.class,
+            //       (v) -> new FileItemStream.ItemSkippedException(v))
+            //   .targetTypeMapping(
+            //       Value.class, LimitedInputStream.class, (v) -> new LimitedInputStream(v))
+            //   .targetTypeMapping(Value.class, ParameterParser.class, (v) -> new ParameterParser(v))
+            //   .targetTypeMapping(
+            //       Value.class, DefaultFileItemFactory.class, (v) -> new DefaultFileItemFactory(v))
+            //   .targetTypeMapping(Value.class, Constants.class, (v) -> new Constants(v))
+            //   .targetTypeMapping(
+            //       Value.class,
+            //       MockPortletActionRequest.class,
+            //       (v) -> new MockPortletActionRequest(v))
+            //   .targetTypeMapping(
+            //       Value.class, FileUploadException.class, (v) -> new FileUploadException(v))
+              // TODO: Add other mappings
+              .build();
 
-    /**
-     * Directory containing the Python package.
-     * TODO: change to actual directory
-     */
-    private static String packageDirectory = "../commons-fileupload-translated/src/";
-
-    /** Polyglot context for evaluating Python */
-    private static Context context;
-
-    static {
-        try {
-            sharedEngine = Engine.create();
-            context = Context.newBuilder("python")
-                    .allowAllAccess(true)
-                    .option("python.PythonPath", packageDirectory)
-                    .engine(sharedEngine)
-                    .build();
-        } catch (Exception e) {
-            System.out.println("[-] " + e);
-        }
+      sharedEngine = Engine.create();
+      context =
+          Context.newBuilder("python")
+              .allowHostAccess(hostAccess)
+              .option("python.PythonPath", packageDirectory)
+              .engine(sharedEngine)
+              .build();
+    } catch (Exception e) {
+      System.out.println("[-] " + e);
     }
+  }
 
-    public static Value[] getPythonClasses(String fileName, String... classNames) {
-        try {
-            File file = new File(codeDirectory, fileName);
-            Source source = Source.newBuilder("python", file).build();
-            assert source != null;
+  public static Value[] getPythonClasses(String fileName, String... classNames) {
+    try {
+      File file = new File(codeDirectory, fileName);
+      Source source = Source.newBuilder("python", file).build();
+      assert source != null;
 
-            context.eval(source);
+      context.eval(source);
 
-            Value[] result = new Value[classNames.length];
-            for (int i = 0; i < classNames.length; i++) {
-                result[i] = context.getBindings("python").getMember(classNames[i]);
-            }
-            return result;
-        } catch (Exception e) {
-            System.out.println("[-] " + e);
-            return null; // ??
-        }
+      Value[] result = new Value[classNames.length];
+      for (int i = 0; i < classNames.length; i++) {
+        result[i] = context.getBindings("python").getMember(classNames[i]);
+      }
+      return result;
+    } catch (Exception e) {
+      System.out.println("[-] " + e);
+      return null; // ??
     }
+  }
 
-    public static Value getPythonClass(String fileName, String className) {
-        return getPythonClasses(fileName, className)[0];
-    }
+  public static Value getPythonClass(String fileName, String className) {
+    return getPythonClasses(fileName, className)[0];
+  }
 }
--- java_projects/glue_code/commons-fileupload/src/main/java/org/apache/commons/fileupload/util/Streams.java
+++ java_projects/glue_code/commons-fileupload/src/main/java/org/apache/commons/fileupload/util/Streams.java
@@ -1,125 +1,48 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.apache.commons.fileupload.util;
 
 import org.apache.commons.fileupload.ContextInitializer;
-import org.apache.commons.fileupload.InvalidFileNameException;
-
-import java.io.InputStream;
-import java.io.OutputStream;
-
 import org.graalvm.polyglot.Value;
 
-/** Utility class for working with streams. */
 public final class Streams {
+  public static final int DEFAULT_BUFFER_SIZE = 8192;
+  private static Value clz = ContextInitializer.getPythonClass("<placeholder>", "Streams");
+  private Value obj;
 
-    /**
-     * Contains a reference to the Python class.
-     */
-    private static Value clz = ContextInitializer.getPythonClass("util/streams.py",
-            "Streams");
+  public Streams(Value obj) {
+    this.obj = obj;
+  }
 
-    /** Private constructor, to prevent instantiation. This class has only static methods. */
-    private Streams() {}
+  public Value getPythonObject() {
+    return obj;
+  }
 
-    /** Default buffer size for use in {@link #copy(InputStream, OutputStream, boolean)}. */
-    public static final int DEFAULT_BUFFER_SIZE = 8192;
+  public static String checkFileName(String fileName) {
+    //
+    // if (fileName != null && fileName.indexOf('\u0000') != -1) {
+    // final StringBuilder sb = new StringBuilder();
+    // for (int i = 0; i < fileName.length(); i++) {
+    // char c = fileName.charAt(i);
+    // switch (c) {
+    // case 0:
+    // sb.append("\\0");
+    // break;
+    // default:
+    // sb.append(c);
+    // break;
+    // }
+    // }
+    // throw new InvalidFileNameException(fileName, "Invalid file name: " + sb);
+    // }
+    // return fileName;
+    //
 
-    /**
-     * Copies the contents of the given {@link InputStream} to the given {@link OutputStream}.
-     * Shortcut for
-     *
-     * <pre>
-     *   copy(pInputStream, pOutputStream, new byte[8192]);
-     * </pre>
-     *
-     * @param inputStream The input stream, which is being read. It is guaranteed, that {@link
-     *     InputStream#close()} is called on the stream.
-     * @param outputStream The output stream, to which data should be written. May be null, in which
-     *     case the input streams contents are simply discarded.
-     * @param closeOutputStream True guarantees, that {@link OutputStream#close()} is called on the
-     *     stream. False indicates, that only {@link OutputStream#flush()} should be called finally.
-     * @return Number of bytes, which have been copied.
-     * @throws IOException An I/O error occurred.
-     */
+    // TODO: Check the type mapping below!
+    return clz.invokeMember("checkFileName", fileName).as(String.class);
+  }
 
-    /**
-     * Copies the contents of the given {@link InputStream} to the given {@link OutputStream}.
-     *
-     * @param inputStream The input stream, which is being read. It is guaranteed, that {@link
-     *     InputStream#close()} is called on the stream.
-     * @param outputStream The output stream, to which data should be written. May be null, in which
-     *     case the input streams contents are simply discarded.
-     * @param closeOutputStream True guarantees, that {@link OutputStream#close()} is called on the
-     *     stream. False indicates, that only {@link OutputStream#flush()} should be called finally.
-     * @param buffer Temporary buffer, which is to be used for copying data.
-     * @return Number of bytes, which have been copied.
-     * @throws IOException An I/O error occurred.
-     */
+  private Streams() {
+    //
 
-    /**
-     * This convenience method allows to read a {@link
-     * org.apache.commons.fileupload.FileItemStream}'s content into a string. The platform's default
-     * character encoding is used for converting bytes into characters.
-     *
-     * @param inputStream The input stream to read.
-     * @see #asString(InputStream, String)
-     * @return The streams contents, as a string.
-     * @throws IOException An I/O error occurred.
-     */
-
-    /**
-     * This convenience method allows to read a {@link
-     * org.apache.commons.fileupload.FileItemStream}'s content into a string, using the given
-     * character encoding.
-     *
-     * @param inputStream The input stream to read.
-     * @param encoding The character encoding, typically "UTF-8".
-     * @see #asString(InputStream)
-     * @return The streams contents, as a string.
-     * @throws IOException An I/O error occurred.
-     */
-
-    /**
-     * Checks, whether the given file name is valid in the sense, that it doesn't contain any NUL
-     * characters. If the file name is valid, it will be returned without any modifications.
-     * Otherwise, an {@link InvalidFileNameException} is raised.
-     *
-     * @param fileName The file name to check
-     * @return Unmodified file name, if valid.
-     * @throws InvalidFileNameException The file name was found to be invalid.
-     */
-    public static String checkFileName(String fileName) {
-        // if (fileName != null && fileName.indexOf('\u0000') != -1) {
-        //     final StringBuilder sb = new StringBuilder();
-        //     for (int i = 0; i < fileName.length(); i++) {
-        //         char c = fileName.charAt(i);
-        //         switch (c) {
-        //             case 0:
-        //                 sb.append("\\0");
-        //                 break;
-        //             default:
-        //                 sb.append(c);
-        //                 break;
-        //         }
-        //     }
-        //     throw new InvalidFileNameException(fileName, "Invalid file name: " + sb);
-        // }
-        // return fileName;
-        return clz.invokeMember("checkFileName", fileName).asString();
-    }
+    this.obj = clz.invokeMember("__init__");
+  }
 }
