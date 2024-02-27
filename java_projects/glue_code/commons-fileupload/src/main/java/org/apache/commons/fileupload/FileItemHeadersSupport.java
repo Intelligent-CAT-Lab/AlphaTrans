8e0a6579cbbe8ab9bbd486ce98109af205349ca0
Mrigank Pawagi <mrigankpawagi@gmail.com>
2024-02-27 20:45:03 +0530

    remove old version

--- java_projects/glue_code/commons-fileupload/src/main/java/org/apache/commons/fileupload/FileItemHeadersSupport.java
+++
@@ -1,8 +0,0 @@
-package org.apache.commons.fileupload;
-
-
-public interface FileItemHeadersSupport {
-  void setHeaders(FileItemHeaders headers);
-
-  FileItemHeaders getHeaders();
-}
--- java_projects/glue_code/commons-fileupload/src/main/java/org/apache/commons/fileupload/ProgressListener.java
+++
@@ -1,6 +0,0 @@
-package org.apache.commons.fileupload;
-
-
-public interface ProgressListener {
-  void update(long pBytesRead, long pContentLength, int pItems);
-}
