4157a0502f0cc91578e88682048ac6a077f7759d
Mrigank Pawagi <mrigankpawagi@gmail.com>
2024-05-09 14:35:07 +0530

    modify script + generate for FileItemHeadersImpl#addHeader

---
+++ java_projects/compositional_glue_tests/commons-fileupload/addHeader/src/main/java/org/apache/commons/fileupload/ContextInitializer.java
@@ -0,0 +1,63 @@
+package org.apache.commons.fileupload;
+
+import java.io.File;
+import org.apache.commons.fileupload.util.FileItemHeadersImpl;
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Engine;
+import org.graalvm.polyglot.HostAccess;
+import org.graalvm.polyglot.Source;
+import org.graalvm.polyglot.Value;
+
+public abstract class ContextInitializer {
+  private static Engine sharedEngine;
+  private static String codeDirectory =
+      "../../../../data/verified_projects/commons-fileupload/src/main/java/org/apache/commons/fileupload/";
+  private static String packageDirectory =
+      "../../../../data/verified_projects/commons-fileupload/addHeader/";
+  private static Context context;
+
+  static {
+    try {
+      HostAccess hostAccess =
+          HostAccess.newBuilder(HostAccess.ALL)
+              .targetTypeMapping(
+                  Value.class, FileItemHeadersImpl.class, null, (v) -> new FileItemHeadersImpl(v))
+              // TODO: Add other mappings
+              .build();
+
+      sharedEngine = Engine.create();
+      context =
+          Context.newBuilder("python")
+              .allowHostAccess(hostAccess)
+              .allowAllAccess(true)
+              .option("python.PythonPath", packageDirectory)
+              .engine(sharedEngine)
+              .build();
+    } catch (Exception e) {
+      System.out.println("[-] " + e);
+    }
+  }
+
+  public static Value[] getPythonClasses(String fileName, String... classNames) {
+    try {
+      File file = new File(codeDirectory, fileName);
+      Source source = Source.newBuilder("python", file).build();
+      assert source != null;
+
+      context.eval(source);
+
+      Value[] result = new Value[classNames.length];
+      for (int i = 0; i < classNames.length; i++) {
+        result[i] = context.getBindings("python").getMember(classNames[i]);
+      }
+      return result;
+    } catch (Exception e) {
+      System.out.println("[-] " + e);
+      return null; // ??
+    }
+  }
+
+  public static Value getPythonClass(String fileName, String className) {
+    return getPythonClasses(fileName, className)[0];
+  }
+}
---
+++ java_projects/compositional_glue_tests/commons-fileupload/addHeader/src/main/java/org/apache/commons/fileupload/util/FileItemHeadersImpl.java
@@ -0,0 +1,68 @@
+package org.apache.commons.fileupload.util;
+
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import org.apache.commons.fileupload.ContextInitializer;
+import org.apache.commons.fileupload.FileItemHeaders;
+import org.graalvm.polyglot.Value;
+
+public class FileItemHeadersImpl implements FileItemHeaders, Serializable {
+  private final Map<String, List<String>> headerNameToValueListMap =
+      new LinkedHashMap<String, List<String>>();
+  private static final long serialVersionUID = -4455695752627032559L;
+  private static Value clz =
+      ContextInitializer.getPythonClass("/util/FileItemHeadersImpl.py", "FileItemHeadersImpl");
+  private Value obj;
+
+  public FileItemHeadersImpl(Value obj) {
+    this.obj = obj;
+  }
+
+  public Value getPythonObject() {
+    return obj;
+  }
+
+  public Iterator<String> getHeaders(String name) {
+
+    String nameLower = name.toLowerCase(Locale.ENGLISH);
+    List<String> headerValueList = headerNameToValueListMap.get(nameLower);
+    if (null == headerValueList) {
+      headerValueList = Collections.emptyList();
+    }
+    return headerValueList.iterator();
+  }
+
+  public Iterator<String> getHeaderNames() {
+
+    return headerNameToValueListMap.keySet().iterator();
+  }
+
+  public String getHeader(String name) {
+
+    String nameLower = name.toLowerCase(Locale.ENGLISH);
+    List<String> headerValueList = headerNameToValueListMap.get(nameLower);
+    if (null == headerValueList) {
+      return null;
+    }
+    return headerValueList.get(0);
+  }
+
+  public synchronized void addHeader(String name, String value) {
+    //
+    // String nameLower = name.toLowerCase(Locale.ENGLISH);
+    // List<String> headerValueList = headerNameToValueListMap.get(nameLower);
+    // if (null == headerValueList) {
+    // headerValueList = new ArrayList<String>();
+    // headerNameToValueListMap.put(nameLower, headerValueList);
+    // }
+    // headerValueList.add(value);
+    //
+
+    this.obj.invokeMember("addHeader", name, value);
+  }
+}
