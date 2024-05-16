062fc541de9b16003bb6e1851b569b522b7243ea
Mrigank Pawagi <mrigankpawagi@gmail.com>
2024-05-16 11:46:58 +0530

    glue: split script into modules

---
+++ src/compositional_glue_tests/constants.py
@@ -0,0 +1,16 @@
+ORIGINAL_DIR = "java_projects/cleaned_final_projects"
+OUTPUT_DIR = "java_projects/compositional_glue_tests"
+TRANSLATION_DIR = "data/verified_projects"
+DIR_DEPTH = "../../../"
+
+main_paths = {
+    "commons-fileupload": "main/java/org/apache/commons/fileupload/",
+    "commons-cli": "main/java/org/apache/commons/cli/",
+    "commons-codec": "main/java/org/apache/commons/codec/",
+    "commons-csv": "main/java/org/apache/commons/csv/",
+    "commons-graph": "main/java/org/apache/commons/graph/",
+    "commons-pool": "main/java/org/apache/commons/pool/",
+    "commons-validator": "main/java/org/apache/commons/validator/",
+    "joda-convert": "main/java/org/joda/convert/",
+    "joda-money": "main/java/org/joda/money/",
+}
