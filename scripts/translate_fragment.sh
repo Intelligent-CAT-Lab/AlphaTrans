97af76a3e2368935ec8104b20b87a2ef1bf09efd
alibrahimzada <alibrahimzada@gmail.com>
2024-05-28 09:25:10 -0500

    update translation bash script

--- scripts/translate_fragment.sh
+++ scripts/translate_fragment.sh
@@ -1,5 +1,12 @@
 
 for project in 'commons-cli' 'commons-codec' 'commons-csv' 'commons-validator' 'commons-fileupload' 'commons-pool' 'commons-graph' 'joda-money' 'joda-convert';
 do
-    python3 src/translation/translate_fragment.py --model_name=codellama-13b-instruct --project_name=$project --from_lang=Java --to_lang=Python --include_call_graph --num_translations=3 --cache_dir=/home/shared/huggingface --translate_test | tee {$project}_test.log
+    python3 src/translation/compositional_translation_validation.py \
+        --model_name=deepseek-coder-33b-instruct \
+        --project_name=$project \
+        --from_lang=Java \
+        --to_lang=Python \
+        --include_call_graph \
+        --cache_dir=/home/shared/huggingface \
+        --translate_main | tee ${project}_main.log
 done
