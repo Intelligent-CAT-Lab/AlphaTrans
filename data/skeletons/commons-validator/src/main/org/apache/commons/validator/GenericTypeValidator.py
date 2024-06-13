3d196eae06ed211caa9982ee3b01824f33c0fa32
alibrahimzada <alibrahimzada@gmail.com>
2024-06-13 14:02:06 -0500

    updated skeletons

--- data/skeletons/commons-validator/src/main/org/apache/commons/validator/GenericTypeValidator.py
+++ data/skeletons/commons-validator/src/main/org/apache/commons/validator/GenericTypeValidator.py
@@ -5,6 +5,7 @@ from src.main.org.apache.commons.validator.GenericValidator import *
 
 # from src.main.org.apache.commons.logging.LogFactory import *
 # from src.main.org.apache.commons.logging.Log import *
+import logging
 import datetime
 import typing
 from typing import *
