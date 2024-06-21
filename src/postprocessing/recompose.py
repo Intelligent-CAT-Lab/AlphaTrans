60aebb86d55d4ea9bb26728f5119fc3de9afb43a
alibrahimzada <alibrahimzada@gmail.com>
2024-06-21 11:08:49 -0500

    update recompose script

--- src/postprocessing/recompose.py
+++ src/postprocessing/recompose.py
@@ -5,20 +5,19 @@ import os
 
 def main(args):
 
-    translation_dir = f'data/translations/{args.model_name}/signature/{args.project_name}'
-    schema_files = os.listdir(translation_dir)
-    recomposed_projects_dir = f'data/recomposed_projects'
-
     total_fragments = 0
     total_unsuccessful = 0
 
-    for schema in schema_files:
+    for schema in os.listdir(args.translation_dir):
 
         if not schema.endswith('_python_partial.json'):
             continue
 
+        if args.file_name != None and f'.{args.file_name}_python_partial.json' not in schema:
+            continue
+
         data = {}
-        with open(f'{translation_dir}/{schema}') as f:
+        with open(f'{args.translation_dir}/{schema}') as f:
             data = json.load(f)
 
         recomposed_file = '\n'.join(data['python_imports'])
@@ -49,14 +48,7 @@ def main(args):
             if 'new' in class_ or '{' in class_: # skip nested and nameless classes
                 continue
 
-            if 'src.test' in schema and class_.endswith('Test'):
-                recomposed_file = recomposed_file.replace('from __future__ import annotations\n', 'from __future__ import annotations\nimport unittest\nimport pytest\n') if 'import unittest\nimport pytest' not in recomposed_file else recomposed_file
-                if '):' not in data['classes'][class_]['python_class_declaration']:
-                    recomposed_file += data['classes'][class_]['python_class_declaration'].replace(':', '(unittest.TestCase):')
-                else:
-                    recomposed_file += data['classes'][class_]['python_class_declaration'].replace('):', ', unittest.TestCase):')
-            else:
-                recomposed_file += data['classes'][class_]['python_class_declaration']
+            recomposed_file += data['classes'][class_]['python_class_declaration']
 
             if data['classes'][class_]['fields'] == {} and data['classes'][class_]['methods'] == {}:
                 recomposed_file += '    pass\n\n'
@@ -101,8 +93,8 @@ def main(args):
             for field in field_order:
 
                 total_fragments += 1
-                if data['classes'][class_]['fields'][field]['translation'] == None:
-                    recomposed_file += '\n'.join([''] + data['classes'][class_]['fields'][field]['partial_translation']).replace('<placeholder>', 'None # LLM could not translate this field')
+                if data['classes'][class_]['fields'][field]['translation'] == []:
+                    recomposed_file += '\n'.join([''] + data['classes'][class_]['fields'][field]['partial_translation'].split('\n')).replace('<placeholder>', 'None # LLM could not translate this field')
                     recomposed_file += '\n'
                     total_unsuccessful += 1
                     continue
@@ -118,7 +110,7 @@ def main(args):
                 for a_class in class_order:
                     if f'{a_class}(' in field_translation:
                         intialize_later_fields.append((field, field_translation.split('=')[0].strip(), field_translation.split('=')[1].strip()))
-                        recomposed_file += data['classes'][class_]['fields'][field]['partial_translation'].replace('<placeholder>', f'None')
+                        recomposed_file += ''.join(data['classes'][class_]['fields'][field]['partial_translation']).replace('<placeholder>', f'None')
                         recomposed_file += '\n'
                         found = True
                 
@@ -132,20 +124,30 @@ def main(args):
             if intialize_later_fields != []:
                 recomposed_file += f'\n\n    @staticmethod\n    def initialize_fields() -> None:\n'
                 for field, field_name, field_value in intialize_later_fields:
-                    recomposed_file += '    ' + data['classes'][class_]['fields'][field]['partial_translation'].replace(f'{field_name}', f'{class_}.{field_name}').replace('<placeholder>', f'{field_value}\n')
+                    recomposed_file += '    ' + ''.join(data['classes'][class_]['fields'][field]['partial_translation']).replace(f'{field_name}', f'{class_}.{field_name}').replace('<placeholder>', f'{field_value}\n')
                     recomposed_file += '\n'
                 class_initialize_methods.append(f'{class_}.initialize_fields()')
             
             for method in data['classes'][class_]['methods']:
 
-                if 'Ignore' in data['classes'][class_]['methods'][method]['annotations']:
+                if 'Ignore' in [x.split('(')[0] for x in data['classes'][class_]['methods'][method]['annotations']]:
                     recomposed_file += '\n    @pytest.mark.skip(reason="Ignore")\n'
 
-                if data['classes'][class_]['methods'][method]['translation'] == None:
+                if 'Test' in [x.split('(')[0] for x in data['classes'][class_]['methods'][method]['annotations']]:
+                    recomposed_file += '\n    @pytest.mark.test\n'
+                
+                if 'Before' in [x.split('(')[0] for x in data['classes'][class_]['methods'][method]['annotations']]:
+                    recomposed_file += '\n    @pytest.mark.before\n'
+                
+                if 'After' in [x.split('(')[0] for x in data['classes'][class_]['methods'][method]['annotations']]:
+                    recomposed_file += '\n    @pytest.mark.after\n'
+
+                if data['classes'][class_]['methods'][method]['translation'] == []:
                     recomposed_file += '\n'.join([''] + data['classes'][class_]['methods'][method]['partial_translation']).replace('pass', 'pass # LLM could not translate this method')
                     recomposed_file += '\n'
                     total_unsuccessful += 1
                     continue
+
                 recomposed_file += '\n'.join(data['classes'][class_]['methods'][method]['translation'])
                 recomposed_file += '\n'
                 total_fragments += 1
@@ -154,7 +156,7 @@ def main(args):
             recomposed_file += f'\n\n{initialize_method}'
 
         import_map = {'urllib': 'import urllib\n', 'sys': 'import sys\n', 'mock': 'import mock\n', 'cmp_to_key': 'from functools import cmp_to_key\n', 'field': 'from dataclasses import field\n',
-                      'random': 'import random\n', 're': 'import re\n', 'locale': 'import locale\n'}
+                      'random': 'import random\n', 're': 'import re\n', 'locale': 'import locale\n', 'copy': 'import copy\n'}
 
         python_imports = data['python_imports']
         for key in import_map:
@@ -163,7 +165,8 @@ def main(args):
                 python_imports.append(import_map[key].strip())
         
         exception_map = {'IllegalArgumentException': 'ValueError', 'RuntimeException': 'RuntimeError', 'UnsupportedOperationException': 'NotImplementedError',
-                         'CloneNotSupportedError': 'NotImplementedError', 'IllegalStateException': 'RuntimeError'}
+                         'CloneNotSupportedError': 'NotImplementedError', 'IllegalStateException': 'RuntimeError', 'UnsupportedEncodingException': 'ValueError',
+                         'UnsupportedEncodingError': 'ValueError'}
         
         for key in exception_map:
             if key in recomposed_file:
@@ -171,33 +174,33 @@ def main(args):
 
         formatted_schema_fname = '.'.join(schema.split('.')[:-1])
         sub_dir = "/".join(formatted_schema_fname.replace(".", "/").split("/")[1:-1])
-        os.makedirs(f'{recomposed_projects_dir}/{args.model_name}/{args.project_name}/{sub_dir}', exist_ok=True)
-        file_path = f"{recomposed_projects_dir}/{args.model_name}/{args.project_name}/{sub_dir}/{formatted_schema_fname.split('.')[-1].replace('_python_partial', '')}.py"
+        os.makedirs(f'{args.output_dir}/{args.model_name}/{args.project_name}/{sub_dir}', exist_ok=True)
+        file_path = f"{args.output_dir}/{args.model_name}/{args.project_name}/{sub_dir}/{formatted_schema_fname.split('.')[-1].replace('_python_partial', '')}.py"
         with open(file_path, 'w') as f:
             f.write(recomposed_file)
         
         os.system(f'python3 -m black {file_path}')
 
     # add __init__.py files for each subdirectory
-    for schema in schema_files:
+    for schema in os.listdir(args.translation_dir):
         formatted_schema_fname = '.'.join(schema.split('.')[:-1])
         sub_dir = "/".join(formatted_schema_fname.replace(".", "/").split("/")[1:-1])
-        os.makedirs(f'{recomposed_projects_dir}/{args.model_name}/{args.project_name}/{sub_dir}', exist_ok=True)
+        os.makedirs(f'{args.output_dir}/{args.model_name}/{args.project_name}/{sub_dir}', exist_ok=True)
 
         sub_dirs = sub_dir.split('/')
         for i in range(len(sub_dirs)):
             current_sub_dir = '/'.join(sub_dirs[:i+1])
-            with open(f'{recomposed_projects_dir}/{args.model_name}/{args.project_name}/{current_sub_dir}/__init__.py', 'w') as f:
+            with open(f'{args.output_dir}/{args.model_name}/{args.project_name}/{current_sub_dir}/__init__.py', 'w') as f:
                 f.write('')
 
-        file_path = f"{recomposed_projects_dir}/{args.model_name}/{args.project_name}/{sub_dir}/__init__.py"
+        file_path = f"{args.output_dir}/{args.model_name}/{args.project_name}/{sub_dir}/__init__.py"
         with open(file_path, 'w') as f:
             f.write('')
     
-    with open(f'{recomposed_projects_dir}/{args.model_name}/{args.project_name}/pytest.ini', 'w') as f:
-        f.write('# pytest.ini\n\n[pytest]\n# Require at least this version of pytest\nminversion = 8.2.1\n\n# Add options to control the pytest output\naddopts = -ra -q --continue-on-collection-errors --tb=short --junitxml=pytest-report.xml\n\n# Define directories to look for tests\ntestpaths = src/test\n\npython_files = *.py\n\npython_classes = *Test\n\npython_functions = test*')
+    with open(f'{args.output_dir}/{args.model_name}/{args.project_name}/pytest.ini', 'w') as f:
+        f.write('# pytest.ini\n\n[pytest]\n# Require at least this version of pytest\nminversion = 8.2.1\n\nmarkers =\n    test: Custom marker for test functions\n\n# Add options to control the pytest output\naddopts = --tb=native --no-header --junit-xml=pytest-report.xml\n\n# Define directories to look for tests\ntestpaths = src/test\n\npython_files = *.py\n\npython_classes = *Test\n\npython_functions = test*')
     
-    with open(f'{recomposed_projects_dir}/{args.model_name}/{args.project_name}/run.sh', 'w') as f:
+    with open(f'{args.output_dir}/{args.model_name}/{args.project_name}/run.sh', 'w') as f:
         f.write('python3 -m pytest\nxmllint --format pytest-report.xml -o pytest-report.xml')
 
     # print(f'total fragments: {total_fragments}, total unsuccessful: {total_unsuccessful}')
@@ -209,5 +212,8 @@ if __name__ == '__main__':
     parser_ = argparse.ArgumentParser(description='Translate java types to python types')
     parser_.add_argument('--project_name', type=str, dest='project_name', help='project name to translate')
     parser_.add_argument('--model_name', type=str, dest='model_name', help='model name to translate')
+    parser_.add_argument('--translation_dir', type=str, dest='translation_dir', help='directory to store translations')
+    parser_.add_argument('--output_dir', type=str, dest='output_dir', help='directory to store recomposed projects')
+    parser_.add_argument('--file_name', type=str, dest='file_name', help='file name to recompose')
     args = parser_.parse_args()
     main(args)
