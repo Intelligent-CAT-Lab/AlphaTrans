fd0e20d2c9397da3e9f817b24e8f8c6a453b4257
alibrahimzada <alibrahimzada@gmail.com>
2024-06-28 15:29:53 -0500

    update skeleton creation bash script

--- scripts/create_skeleton.sh
+++ scripts/create_skeleton.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+
 pwd=$(pwd)
-for project in 'commons-cli' 'commons-codec' 'commons-csv' 'commons-validator' 'commons-fileupload' 'commons-pool' 'commons-graph' 'joda-money' 'joda-convert';
+
+for model in 'deepseek-coder-33b-instruct' 'granite-34b-code-instruct';
 do
-    echo "creating skeleton for $project"
-    export PYTHONPATH=$pwd/data/skeletons/$project
-    python3 src/static_analysis/create_skeleton.py --project_name=$project
+    for type in 'body' 'signature';
+    do
+        for project in 'commons-cli' 'commons-codec' 'commons-csv' 'commons-validator' 'commons-fileupload' 'commons-pool' 'commons-graph' 'joda-money' 'joda-convert';
+        do
+            echo "creating skeleton for $project"
+            export PYTHONPATH=$pwd/data/skeletons/$project
+            python3 src/static_analysis/create_skeleton.py --project_name=$project --model_name=$model --type=$type
+        done
+    done
 done
