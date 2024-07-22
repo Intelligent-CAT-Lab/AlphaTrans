9c6983bef0853c792a571515d20d4ef1408b03a7
Mrigank Pawagi <mrigankpawagi@gmail.com>
2024-07-22 00:24:28 -0500

    glue fixes

--- src/compositional_glue_tests/misc/ContextInitializer.java
+++ src/compositional_glue_tests/misc/ContextInitializer.java
@@ -8,50 +8,49 @@ import org.graalvm.polyglot.Source;
 import org.graalvm.polyglot.Value;
 
 public abstract class ContextInitializer {{
-    private static Engine sharedEngine;
-    private static String codeDirectory = "{code_directory}";
-    private static String testDirectory = "{test_directory}";
-    private static String packageDirectory = "{package_directory}";
-    private static Context context;
-    static {{
-        try {{
-            HostAccess hostAccess = HostAccess.newBuilder(HostAccess.ALL)
-{mappings}
-                    .build();
+  private static Engine sharedEngine;
+  private static String srcDirectory = "{src_directory}";
+  private static String packageDirectory = "{package_directory}";
+  private static Context context;
 
-            sharedEngine = Engine.create();
-            context = Context.newBuilder("python")
-                    .allowHostAccess(hostAccess)
-                    .allowAllAccess(true)
-                    .option("python.PythonPath", packageDirectory)
-                    .engine(sharedEngine)
-                    .build();
-        }} catch (Exception e) {{
-            System.out.println("[-] " + e);
-        }}
-    }}
-    public static Value[] getPythonClasses(String fileName, boolean isInTestDirectory, String... classNames) {{
-        try {{
-            File file = isInTestDirectory ? new File(testDirectory, fileName) : new File(codeDirectory, fileName);
-            Source source = Source.newBuilder("python", file).build();
-            assert source != null;
+  static {{
+    try {{
+      HostAccess hostAccess = HostAccess.newBuilder(HostAccess.ALL).build();
 
-            context.eval(source);
+      sharedEngine = Engine.create();
+      context =
+          Context.newBuilder("python")
+              .allowHostAccess(hostAccess)
+              .allowAllAccess(true)
+              .option("python.PythonPath", packageDirectory)
+              .engine(sharedEngine)
+              .build();
+    }} catch (Exception e) {{
+      System.out.println("[-] " + e);
+    }}
+  }}
 
-            Value[] result = new Value[classNames.length];
-            for (int i = 0; i < classNames.length; i++) {{
-                result[i] = context.getBindings("python").getMember(classNames[i]);
-            }}
-            return result;
-        }} catch (Exception e) {{
-            System.out.println("[-] In File " + fileName + ": " + e);
-            return null; // ??
-        }}
+  public static Value[] getPythonClasses(
+      String filePath, String... classNames) {{
+    try {{
+      File file = new File(srcDirectory, filePath);
+      Source source = Source.newBuilder("python", file).build();
+      assert source != null;
+
+      context.eval(source);
+
+      Value[] result = new Value[classNames.length];
+      for (int i = 0; i < classNames.length; i++) {{
+        result[i] = context.getBindings("python").getMember(classNames[i]);
+      }}
+      return result;
+    }} catch (Exception e) {{
+      System.out.println("[-] In File " + filePath + ": " + e);
+      return null;
     }}
-    public static Value getPythonClass(String fileName, String className, boolean isInTestDirectory) {{
-        return getPythonClasses(fileName, isInTestDirectory, className)[0];
-    }}
-    public static Value getPythonClass(String fileName, String className) {{
-        return getPythonClass(fileName, className, false);
-    }}
+  }}
+
+  public static Value getPythonClass(String filePath, String className) {{
+    return getPythonClasses(filePath, className)[0];
+  }}
 }}
