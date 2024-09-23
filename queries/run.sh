9875e50f9ca572973970b9aeb0b54046f440ee62
alibrahimzada <alibrahimzada@gmail.com>
2024-09-22 19:41:15 -0500

    update

--- queries/run.sh
+++ queries/run.sh
@@ -1,10 +1,10 @@
-bash simplify_script.sh trying_stuff_out trying_stuff_out
-# bash simplify_script.sh commons-cli commons-cli
-# bash simplify_script.sh commons-codec commons-codec
-# bash simplify_script.sh commons-csv commons-csv
-# bash simplify_script.sh commons-validator commons-validator
-# bash simplify_script.sh commons-fileupload commons-fileupload
-# bash simplify_script.sh commons-pool commons-pool
-# bash simplify_script.sh commons-graph commons-graph
-# bash simplify_script.sh joda-money joda-money
-# bash simplify_script.sh joda-convert joda-convert
+bash simplify_script.sh commons-cli commons-cli
+bash simplify_script.sh commons-codec commons-codec
+bash simplify_script.sh commons-csv commons-csv
+bash simplify_script.sh commons-validator commons-validator
+bash simplify_script.sh commons-fileupload commons-fileupload
+bash simplify_script.sh commons-pool commons-pool
+bash simplify_script.sh commons-graph commons-graph
+bash simplify_script.sh commons-exec commons-exec
+bash simplify_script.sh jansi jansi
+bash simplify_script.sh JavaFastPFOR JavaFastPFOR
